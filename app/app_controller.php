ce01cfba1d7a1cd51a6eaa87516e76f08beee390
Rob Wilkerson <rob@robwilkerson.org>
2011-07-15 07:22:42 -0400

    Adding useful crap to the App(Model|Controller).
    Includes common components, helpers, behaviors as well as a number of
    custom methods, overridden methogs and callbacks.

--- app/app_controller.php
+++ app/app_controller.php
@@ -1,36 +1,100 @@
 <?php
-/**
- * Application level Controller
- *
- * This file is application-wide controller file. You can put all
- * application-wide controller-related methods here.
- *
- * PHP versions 4 and 5
- *
- * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
- * Copyright 2005-2010, Cake Software Foundation, Inc. (http://cakefoundation.org)
- *
- * Licensed under The MIT License
- * Redistributions of files must retain the above copyright notice.
- *
- * @copyright     Copyright 2005-2010, Cake Software Foundation, Inc. (http://cakefoundation.org)
- * @link          http://cakephp.org CakePHP(tm) Project
- * @package       cake
- * @subpackage    cake.cake.libs.controller
- * @since         CakePHP(tm) v 0.2.9
- * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
- */
 
-/**
- * This is a placeholder class.
- * Create the same file in app/app_controller.php
- *
- * Add your application-wide methods in the class below, your controllers
- * will inherit them.
- *
- * @package       cake
- * @subpackage    cake.cake.libs.controller
- * @link http://book.cakephp.org/view/957/The-App-Controller
- */
 class AppController extends Controller {
+  public $helpers    = array( 'Html', 'Number', 'Session', 'Text', 'Time' );
+  public $components = array( 'Auth', 'RequestHandler', 'Session' );
+  
+  /**
+   * OVERRIDES
+   */
+  
+  /**
+   * Override this method to ensure that some components get loaded
+   * conditionally.
+   *
+   * @access	public
+   */
+  public function constructClasses() {
+    if( Configure::read( 'debug' ) > 0 ) {
+      $this->components[] = 'DebugKit.Toolbar';
+    }
+    
+    parent::constructClasses();
+  }
+  
+  /**
+   * PROTECTED METHODS
+   */
+  
+  /**
+   * Returns the current user object. Used to support the Auditable
+   * behavior for delete actions which send no data, but perform a
+   * soft delete by updating the active value.
+   *
+   * @return  array
+   * @access  protected
+   */
+  protected function current_user( $property = null ) {
+    $user = $this->Auth->user();
+    
+    if( !empty( $user ) ) {
+      if( empty( $property ) ) {
+        $user = $user[$this->Auth->userModel]; # Return the complete user array
+      }
+      else {
+        $user = $this->Auth->user( $property ); # Return a specific property
+      }
+    }
+    
+    return $user;
+  }
+  
+  /**
+   * Refreshes the authenticated user session partially or en masse.
+   *
+   * @param   $field
+   * @param   $value
+   * @return  boolean
+   * @see     http://milesj.me/blog/read/31/Refreshing-The-Auths-Session
+   */
+  protected function refresh_auth( $field = null, $value = null ) {
+    if( $this->Auth->user() ) {
+      if( !empty( $field ) && !empty( $value ) ) { # Refresh a single key
+        $this->Session->write( $this->Auth->sessionKey . '.' . $field, $value );
+      }
+      else { # Refresh the entire session
+        $user = ClassRegistry::init( $this->Auth->userModel )->find(
+          'first',
+          array(
+            'contain'    => false,
+            'conditions' => array( 'User.id' => $this->Auth->User( 'id' ) ),
+          )
+        );
+        
+        $this->Auth->login( $user );
+      }
+    }
+  }
+  
+  /**
+   * PRIVATE METHODS
+   */
+  
+  /**
+   * Force traffic to a given action through SSL.
+   */
+  private function forceSSL() {
+    if( !$this->RequestHandler->isSSL() ) {
+      $this->redirect( 'https://' . $_SERVER['HTTP_HOST'] . $this->here, null, true );
+    }
+  }
+  
+  /**
+   * Force traffic to a given action away from SSL.
+   */
+  private function unforceSSL() {
+    if( $this->RequestHandler->isSSL() ) {
+      $this->redirect( 'http://' . $_SERVER['HTTP_HOST'] . $this->here, null, true );
+    }
+  }
 }
--- app/app_model.php
+++ app/app_model.php
@@ -1,35 +1,116 @@
 <?php
-/**
- * Application model for Cake.
- *
- * This file is application-wide model file. You can put all
- * application-wide model-related methods here.
- *
- * PHP versions 4 and 5
- *
- * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
- * Copyright 2005-2010, Cake Software Foundation, Inc. (http://cakefoundation.org)
- *
- * Licensed under The MIT License
- * Redistributions of files must retain the above copyright notice.
- *
- * @copyright     Copyright 2005-2010, Cake Software Foundation, Inc. (http://cakefoundation.org)
- * @link          http://cakephp.org CakePHP(tm) Project
- * @package       cake
- * @subpackage    cake.cake.libs.model
- * @since         CakePHP(tm) v 0.2.9
- * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
- */
 
-/**
- * Application model for Cake.
- *
- * This is a placeholder class.
- * Create the same file in app/app_model.php
- * Add your application-wide methods to the class, your models will inherit them.
- *
- * @package       cake
- * @subpackage    cake.cake.libs.model
- */
 class AppModel extends Model {
+  public $actsAs = array( 'Nullable', 'Containable' );
+  
+  /**
+   * OVERRIDES
+   */
+  
+	/**
+	 * Override Model::deconstruct() in order to use an integrated date
+	 * value, but multipart time value. The parent method expects both
+	 * date and time to be segmented, but, if a date picker is used to
+	 * select the date, then that component is unified.
+	 *
+	 * In order to use what's already in place, we'll deconstruct the date
+	 * portion here and then pass everything to the parent method for
+	 * reassimilation.
+	 *
+	 * @param		string	$field 	The name of the field to be deconstructed
+	 * @param		mixed 	$data 	An array or object to be deconstructed into a field
+	 * @return	mixed						The resulting data that should be assigned to a field
+	 * @access  protected
+	 */
+	public function deconstruct( $field, $data ) {
+		$type = $this->getColumnType( $field );
+		
+		if( in_array( $type, array( 'datetime', 'timestamp' ) ) ) {
+			if( isset( $data['date'] ) && !empty( $data['date'] ) ) {
+				$date = date( 'U', strtotime( $data['date'] ) );
+				
+				if( $date ) {
+					$data['month'] = date( 'm', $date );
+					$data['day']   = date( 'd', $date );
+					$data['year']  = date( 'Y', $date );
+				}
+			}
+		}
+		
+		return parent::deconstruct( $field, $data );
+	}
+  
+  /**
+   * VALIDATORS
+   */
+  
+	/**
+	 * Validates a datetime value by acting as a decorator for native
+	 * Validation::date() and Validation::time() methods.
+	 *
+	 * @param		$check		array		field_name => value
+	 * @param		$options	array		Options for this rule
+	 * @return	boolean
+	 * @access  public
+	 */
+	public function datetime( $check, $options ) {
+		$check    = array_shift( array_values( $check ) );
+		$datetime = strtotime( $check );
+		
+		if( $datetime !== false ) {
+			return Validation::date( date( 'Y-m-d', $datetime ), 'ymd' ) && Validation::time( date( 'H:i', $datetime ) );
+		}
+		
+		return false;
+	}
+  
+  /**
+   * Custom validation method specific to integers.
+   *
+   * @param   $check
+   * @access  public
+   */
+  public function integer( $check = array() ) {
+    $value = array_shift( array_values( $check ) );
+    
+    return preg_match( '/^\d+$/', $value );
+  }
+  
+  /**
+   * PUBLIC METHODS
+   */
+  
+	/**
+	 * Returns the current user object/array. Useful in the context of
+	 * the Auditable behavior.
+	 *
+	 * @return		mixed   null if empty,
+	 *                    an array if no property is specified,
+	 *                    a scalar if a property is specified
+	 * @access    public
+	 * @todo      Detect Auth user model automatically?
+	 */
+	public function current_user( $property = null ) {
+		$user = isset( $this->data['User'] )
+      ? $this->data['User']
+      : null;
+    
+    # Pull just the property, if specified
+    if( !empty( $property ) ) {
+      $user = $user[$property];
+    }
+    
+    return $user;
+	}
+  
+  /**
+   * Returns the generated SQL executed during the request.
+   * Useful when debugging.
+   *
+   * @return array
+   * @access public
+   */
+  public function sql() {
+    return $this->getDataSource()->getLog( false, false );
+  }
 }
