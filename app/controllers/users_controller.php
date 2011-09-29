c5222e8dca1a9139db93e93aa086983027969176
Rob Wilkerson <rob@robwilkerson.org>
2011-09-29 10:17:21 -0400

    Destroying the session.
    I always assumed that Cake would do that when Auth::logout() was called. It
    does not.

--- app/controllers/users_controller.php
+++ app/controllers/users_controller.php
@@ -144,6 +144,7 @@ class UsersController extends AppController {
    * @access	public
    */
   public function logout() {
+    $this->Session->destroy();
     $this->redirect( $this->Auth->logout(), null, true );
   }
 }
